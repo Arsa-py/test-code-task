ff7f066e1f198b854a592a9514afa0252a9ca3a4
Arsa-py <kraljarsa991@gmail.com>
2022-04-04 20:38:46 +0200

    Xmas commission has been added

--- GuestReady_commission/src/gr_commission.py
+++ GuestReady_commission/src/gr_commission.py
@@ -10,6 +10,8 @@ print()
 def get_commission(date_str):
     dt = str(input(date_str))
     high_season = range(6, 10, 1)
+    xmas_season_dec = 12
+    xmas_season_jan = 1
     try:
         # date object
         date_obj = datetime.strptime(dt, '%Y-%m-%d').date()
@@ -19,6 +21,12 @@ def get_commission(date_str):
         if mnth in high_season:
             print(f'The GuestReady commission for {date_obj} is 15%')
             return 0.15
+        elif mnth is xmas_season_dec:
+            print(f'The GuestReady commission for {date_obj} is 12%')
+            return 0.12
+        elif mnth is xmas_season_jan:
+            print(f'The GuestReady commission for {date_obj} is 13%')
+            return 0.13
         else:
             print(f'The GuestReady commission for {date_obj} is 10%')
             return 0.10
--- GuestReady_commission/src/test_gr_commission.py
+++ GuestReady_commission/src/test_gr_commission.py
@@ -13,5 +13,13 @@ class TestGetCommission(unittest.TestCase):
     def test_low_season_commission(self, mock_print, mock_input):
         self.assertEqual(get_commission('2012-10-07'), 0.10)
 
+    @patch('gr_commission.input', return_value='2012-12-25')
+    def test_xmas_dec_commission(self, mock_print, mock_input):
+        self.assertEqual(get_commission('2012-12-25'), 0.12)
+
+    @patch('gr_commission.input', return_value='2012-01-07')
+    def test_xmas_jan_commission(self, mock_print, mock_input):
+        self.assertEqual(get_commission('2012-01-07'), 0.13)
+
 if __name__ == "__main__":
     unittest.main()
